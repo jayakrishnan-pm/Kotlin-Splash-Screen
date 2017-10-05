579cb892d08ef68eb3191ddfa6ee368a6c59e988
Jayakrishnan PM <jayakrishnan.pm.123@gmail.com>
2017-10-05 22:30:51 +0530

    first commit

---
+++ app/src/main/java/devdeeds/com/kotlinsplashscreen/HomeActivity.kt
@@ -0,0 +1,17 @@
+package devdeeds.com.kotlinsplashscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * A sample splash screen created by devdeeds.com
+ * by Jayakrishnan P.M
+ */
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/devdeeds/com/kotlinsplashscreen/SplashActivity.kt
@@ -0,0 +1,46 @@
+package devdeeds.com.kotlinsplashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * A sample splash screen created by devdeeds.com
+ * by Jayakrishnan P.M
+ */
+class SplashActivity : AppCompatActivity() {
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
+}
